fa632a96b0423ed1bee2d2c7bd54c4018bb4caa0
Robs68 <robin.irlinger@gmail.com>
2019-12-19 23:03:42 +0100

    m0j

--- doublons_films.py
+++ doublons_films.py
@@ -7,33 +7,52 @@ from fuzzywuzzy import process
 print("Lancement du script\n")
 
 def to_csv() :
-    file1_txt = open('liste_films1.txt','rb')
-#file1 = unicode(file1, error='replace')
-    df=pd.read_fwf('liste_films1.txt')
-    df.to_csv('liste_films1.csv')
-    df=pd.read_csv('liste_films1.csv')
-    df.columns=["nombre","date","heure","tutu2","noms films", "suppr", "suppr2"]
-    df=df.drop(["suppr","tutu2","suppr2"], axis=1)
+    file1_txt = open('liste_films2.txt','rb')
+    df2=pd.read_fwf('liste_films2.txt')
+    df2.to_csv('liste_films2.csv')
+#    df=pd.read_csv('liste_films1.csv')
+#    df.columns=["nombre","date","heure","tutu2","noms films", "suppr", "suppr2"]
+#    df=df.drop(["suppr","tutu2","suppr2"], axis=1)
 
-def fuz(a):
-   ratio=fuzz.token_sort_ratio(df['noms films']
 
 #to_csv()
 df=pd.read_csv('liste_films1.csv')
 df.columns=["nombre","date","heure","tutu2","noms films", "suppr", "suppr2"]
-df=df.drop(["suppr","tutu2","suppr2"], axis=1)
+df=df.drop(["nombre","date","heure","tutu2", "suppr", "suppr2"], axis=1)
+print (df)
 
-csv_1=df['noms films'].values
+df2=pd.read_csv('liste_films2.csv')
+df2.columns=["nombre","date","heure","tutu2","noms films"]
+df2=df2.drop(["nombre","date","heure","tutu2"], axis=1)
+print (df2)
 
-print ("find duplicate Panda\n")
+#csv_1=df['noms films'].values
 
-duplicate=df[df.duplicated(['noms films'])]
-print (duplicate)
+#print ("find duplicate Panda\n")
 
-print ("find duplicate fuzzy")
-print ("")
+#duplicate=df[df.duplicated(['noms films'])]
+#print (duplicate)
 
-ratio=fuzz.token_sort_ratio("string","string2")
-print (ratio)
+print ("find duplicate fuzzy\n")
+
+#df2=df.copy()
+#print (df2)
+compare = pd.MultiIndex.from_product([df['noms films'],df2['noms films']]).to_series()
 
 
+def metrics(tup):
+    return pd.Series([fuzz.ratio(*tup),
+                      fuzz.token_sort_ratio(*tup)],
+                     ['ratio', 'token'])
+
+matched_vendors = []
+
+for row in df.index:
+    noms1 = df.get_value(row,"noms films")
+#    noms1 = df.at['noms films']
+    for columns in df2.index:
+        noms2=df2.get_value(columns,"noms films")
+        matched_token=fuzz.token_sort_ratio(noms1,noms2)
+        if matched_token> 85:
+            matched_vendors.append([noms1,noms2,matched_token])
+print (matched_vendors)
