b66c84f4be4b077c20c90d2778a5fa97cdbce446
robs68 <Tata92310>
2020-05-22 20:25:35 +0200

    Modify

--- doublons_films.py
+++ doublons_films.py
@@ -24,6 +24,20 @@ def pattern(df):
     df['Noms'] = df['Noms'].str.replace(".avi", "")
     df['Noms'] = df['Noms'].str.replace(".mkv", "")
     df['Noms'] = df['Noms'].str.replace(".WEBRip.", "")
+    df['Noms'] = df['Noms'].str.replace('.x264 - PRiDEHD', "")
+    df['Noms'] = df['Noms'].str.replace(".WEBRiP.-ReBoT", "")
+    df['Noms'] = df['Noms'].str.replace(".x264 - NEO", "")
+    df['Noms'] = df['Noms'].str.replace('1080p.WEB.H264 - PREUMS', "")
+    df['Noms'] = df['Noms'].str.replace('1080p.WEB.H264 - D0LL4R', "")
+    df['Noms'] = df['Noms'].str.replace('1080p.WEB.H264-D0LL4R', "")
+    df['Noms'] = df['Noms'].str.replace('1080p.WEB - DL.DD5.1.H264 - FRATERNiTY', "")
+    df['Noms'] = df['Noms'].str.replace('x264 - PRiDEHD', "")
+    df['Noms'] = df['Noms'].str.replace('SUBFORCED.BRRip.x264. - FUNKY', "")
+    df['Noms'] = df['Noms'].str.replace('SUBFORCED.BRRip.x264.-FUNKY', "")
+    df['Noms'] = df['Noms'].str.replace('BRRip.x264.-FUNKY', "")
+
+
+
 
 
 def fuzzy(df, df_copy, films):
@@ -34,13 +48,14 @@ def fuzzy(df, df_copy, films):
         for f in range(longeur2):
             noms_2 = df_copy['Noms'][f]
             ratio = fuzz.ratio(noms_1, noms_2)
-            if ratio > 85:
+            if ratio > 75 and ratio != 100:
                 # films.append("Doublon {} || {} ==>  ratio de {}".format(noms_1, noms_2, ratio))
                 print("Doublon {} || {} ==>  ratio de {}".format(noms_1, noms_2, ratio))
                 with open('Doublons.txt', 'a') as the_file:
                     the_file.write("Doublon {} || {} ==>  ratio de {}".format(noms_1, noms_2, ratio) + '\n')
 
     return films
+
 
 
 pattern(df)
@@ -52,6 +67,7 @@ films = []
 films2 = []
 films = fuzzy(df, df_copy, films)
 films2 = fuzzy(df2, df2_copy, films2)
-films = fuzzy(df, df2, films)
+#films = fuzzy(df, df2, films)
+
 
 print("Script fini")
