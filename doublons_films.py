323db2cc1bc405d0415bf0ed9ae92026d4ccab9e
robs68 <Tata92310>
2020-05-22 16:46:12 +0200

    Amélioration

--- doublons_films.py
+++ doublons_films.py
@@ -2,57 +2,56 @@
 
 import pandas as pd
 from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
 
 print("Lancement du script\n")
 
-def to_csv() :
-    file1_txt = open('liste_films2.txt','rb')
-    df2=pd.read_fwf('liste_films2.txt')
-    df2.to_csv('liste_films2.csv')
-#    df=pd.read_csv('liste_films1.csv')
-#    df.columns=["nombre","date","heure","tutu2","noms films", "suppr", "suppr2"]
-#    df=df.drop(["suppr","tutu2","suppr2"], axis=1)
+df = pd.read_fwf('films_1.txt', names=["Noms"], skip_blank_lines=True)
+df2 = pd.read_fwf('films_2.txt', names=["Noms"], skip_blank_lines=True)
 
 
-#to_csv()
-df=pd.read_csv('liste_films1.csv')
-df.columns=["nombre","date","heure","tutu2","noms films", "suppr", "suppr2"]
-df=df.drop(["nombre","date","heure","tutu2", "suppr", "suppr2"], axis=1)
-print (df)
-
-df2=pd.read_csv('liste_films2.csv')
-df2.columns=["nombre","date","heure","tutu2","noms films"]
-df2=df2.drop(["nombre","date","heure","tutu2"], axis=1)
-print (df2)
-
-#csv_1=df['noms films'].values
-
-#print ("find duplicate Panda\n")
-
-#duplicate=df[df.duplicated(['noms films'])]
-#print (duplicate)
-
-print ("find duplicate fuzzy\n")
-
-#df2=df.copy()
-#print (df2)
-compare = pd.MultiIndex.from_product([df['noms films'],df2['noms films']]).to_series()
+def pattern(df):
+    df['Noms'] = df['Noms'].str.replace("FRENCH", "")
+    df['Noms'] = df['Noms'].str.replace("BDRip", "")
+    df['Noms'] = df['Noms'].str.replace("BDRiP", "")
+    df['Noms'] = df['Noms'].str.replace("x624", "")
+    df['Noms'] = df['Noms'].str.replace("XviD", "")
+    df['Noms'] = df['Noms'].str.replace("BluRay", "")
+    df['Noms'] = df['Noms'].str.replace("DVDRiP", "")
+    df['Noms'] = df['Noms'].str.replace("Multi", "")
+    df['Noms'] = df['Noms'].str.replace("DTS", "")
+    df['Noms'] = df['Noms'].str.replace("AC3", "")
+    df['Noms'] = df['Noms'].str.replace("French", "")
+    df['Noms'] = df['Noms'].str.replace(".avi", "")
+    df['Noms'] = df['Noms'].str.replace(".mkv", "")
+    df['Noms'] = df['Noms'].str.replace(".WEBRip.", "")
 
 
-def metrics(tup):
-    return pd.Series([fuzz.ratio(*tup),
-                      fuzz.token_sort_ratio(*tup)],
-                     ['ratio', 'token'])
+def fuzzy(df, df_copy, films):
+    longeur = df.shape[0]
+    longeur2 = df_copy.shape[0]
+    for i in range(longeur):
+        noms_1 = (df['Noms'][i])
+        for f in range(longeur2):
+            noms_2 = df_copy['Noms'][f]
+            ratio = fuzz.ratio(noms_1, noms_2)
+            if ratio > 85:
+                # films.append("Doublon {} || {} ==>  ratio de {}".format(noms_1, noms_2, ratio))
+                print("Doublon {} || {} ==>  ratio de {}".format(noms_1, noms_2, ratio))
+                with open('Doublons.txt', 'a') as the_file:
+                    the_file.write("Doublon {} || {} ==>  ratio de {}".format(noms_1, noms_2, ratio) + '\n')
 
-matched_vendors = []
+    return films
 
-for row in df.index:
-    noms1 = df.get_value(row,"noms films")
-#    noms1 = df.at['noms films']
-    for columns in df2.index:
-        noms2=df2.get_value(columns,"noms films")
-        matched_token=fuzz.token_sort_ratio(noms1,noms2)
-        if matched_token> 85:
-            matched_vendors.append([noms1,noms2,matched_token])
-print (matched_vendors)
+
+pattern(df)
+pattern(df2)
+df_copy = df
+df2_copy = df2
+
+films = []
+films2 = []
+films = fuzzy(df, df_copy, films)
+films2 = fuzzy(df2, df2_copy, films2)
+films = fuzzy(df, df2, films)
+
+print("Script fini")
